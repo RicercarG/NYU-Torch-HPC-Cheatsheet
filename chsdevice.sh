2cc27349f753de4b3894ed3842eb01af1c22b37f
Yuanhe Guo <yg2709@torch-login-0.hpc.nyu.edu>
2026-02-05 09:42:44 -0500

    replace srun with sbatch for interactive runtime, credit to @JunhaoZhu0220 and @JimmmmmL

--- chsdevice.sh
+++ chsdevice.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# NOTE THAT currently srun doesn't work properly
+# As a work around, we use the sbatch command, and jump to the allocated node
+# This script will let sbatch behave exactly the same as srun
+
 script_dir=$(dirname "$(realpath "$0")")
 
 is_integer() {
@@ -61,7 +65,7 @@ if [ "$num_cpu" -ne 0 ]; then
   fi
 
   # write the base command
-  cmd="srun --nodes=1 --cpus-per-task="$num_cpu" --mem="$mem"GB --time="$time":00:00"
+  cmd="sbatch --nodes=1 --tasks-per-node=1 --cpus-per-task="$num_cpu" --mem="$mem"GB --time="$time":00:00"
 
   # request for gpu if necessary
   if [ "$num_gpu" -ne 0 ]; then
@@ -74,11 +78,36 @@ if [ "$num_cpu" -ne 0 ]; then
     echo -e "${GREEN}Requesting device with project account $account ${NC}"
   fi
 
-  # add interactive flag
-  cmd="$cmd --pty /bin/bash"
+  # we don't want to output loggings into files
+  cmd="$cmd --output=/dev/null --error=/dev/null --parsable --wrap \"sleep infinity\""
 
   # run the final command to ask for devices
-  eval "$cmd"
+  JOB_ID=$(eval "$cmd")
+
+  echo "Job $JOB_ID queued and waiting for resources"
+  # echo "Job $JOB_ID submitted. Waiting for device allocation..."
+
+  # loop until a node is assigned
+  while :; do
+    NODE=$(squeue -j "$JOB_ID" -h -o %N)
+
+    # Check if a valid node name is returned
+    if [ ! -z "$NODE" ] && [ "$NODE" != "n/a" ] && [ "$NODE" != "None" ]; then
+      echo "Job $JOB_ID has been allocated resources"
+
+      # Disable the previous trap so Ctrl+C works inside SSH
+      trap - SIGINT
+
+      # 3. SSH directly into the node (Interactive Shell)
+      ssh "$NODE"
+
+      break
+    fi
+    sleep 3
+  done
+
+  # kill the job if exited manually from the SSH session
+  scancel "$JOB_ID"
 
 else
   echo "Device allocation is skipped."
